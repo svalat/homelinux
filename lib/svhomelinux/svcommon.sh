771ec5faad895a9be652efe127a92ed5582959eb
Valat Sébastien <sebastien.valat.dev@orange.fr>
2011-04-03 13:08:32 +0200

    * first commit

---
+++ lib/svhomelinux/svcommon.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+##############################################################
+#                                                            #
+#         PROJECT : svHomeLinux                              #
+#         VERSION : 1.1                                      #
+#         DATE    : jeu. févr.  3 18:53:24 CET 2011          #
+#         AUTHORS : Sébastien Valat                          #
+#         LICENCE : GPLv2                                    #
+#                                                            #
+##############################################################
+
+######################### SECTION ############################
+SV_HOME_LINUX_SHARED="${PREFIX}/share/svhomelinux"
+SV_HOME_LINUX_INSTALLED="${SV_HOME_LINUX_SHARED}/installed"
+SV_HOME_LINUX_SHARED_NP="./share/svhomelinux"
+SV_HOME_LINUX_INSTALLED_NP="${SV_HOME_LINUX_SHARED_NP}/installed"
+SV_HOME_LINUX_DISTFILES="${SV_HOME_LINUX_SHARED}/distfiles"
+
+######################### FUNCTION ###########################
+function safe_exec()
+{
+	echo "$*"
+	$* || exit 1
+}
+
+######################### FUNCTION ###########################
+#Check if the package is alreay installed and do error if true
+#Params :
+#    $1 : package name (or package-version)
+#    $2 : package version
+function error_if_package_is_installed()
+{
+	echo ">>> Check if already installed <<<"
+	if [ -z "$2" ]; then
+		pname="$1"
+	else
+		pname="$1-$2"
+	fi
+	if [ -f "${SV_HOME_LINUX_INSTALLED}/${pname}.flist" ]
+	then
+		echo "ERROR : ${pname} alredy installed, please remove first"
+		exit 1
+	fi
+}
+
+######################### FUNCTION ###########################
+# Return the value of arguement considering the format --argname=value
+# Params:
+#   - complete string
+#   - argname
+function extract_argument()
+{
+	echo "$1" | sed -e "s/--$2=//g"
+}
+
+######################### FUNCTION ###########################
+# Provide a Yes/No question an return 0 for yes, 1 for no
+# Params:
+#    $1 : Define the question to ask
+#    $2 : Define the default answer ('yes' or 'no')
+function yes_no_question()
+{
+	res=$2
+	tmp=''
+	while [ -z "$res" ]; do
+		if [ "$2" ]; then
+			printf "$1 ? (Y/n) "
+		else
+			printf "$1 ? (y/N) "
+		fi
+		read res
+		if [ -z "$res" ]; then
+			res="$2"
+		elif [ "$rep" != "y" ] || [ "$rep" != "Y" ]; then
+			res='yes'
+		elif [ "$rep" = 'n' ] || [ "$rep" = "N" ]; then
+			res='no'
+		fi
+	done
+
+	if  [ "$res" = "yes" ]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+######################### SECTION ############################
+function yumCheckIfInstall()
+{
+	if [ `ls ${SV_HOME_LINUX_INSTALLED}/${1}-*.flist 2>/dev/null | wc -l` -eq 0 ]; then
+		return 1
+	else
+		return 0
+	fi
+}
+
+######################### FUNCTION ###########################
+# Definit un héritage d'une librairie de base
+# Params :
+#     $1 : lib name (withour the .sh)
+function inherit()
+{
+	#check config file
+	if [ ! -f ${PREFIX}/lib/svhomelinux/$1.sh ]; then
+		echo "Error: Can't find ${PREFIX}/lib/svhomelinux/$1.sh file"
+		exit 1
+	fi
+	source ${PREFIX}/lib/svhomelinux/$1.sh || exit 1
+}
+
+######################### SECTION ############################
+#{name}
+function pack_var_required()
+{
+	echo "The package require the definition of variable $1"
+	exit 1
+}
+
+######################### SECTION ############################
+#{name}
+function yumFindLastVersion()
+{
+	res="`zcat ${SV_HOME_LINUX_SHARED}/distfile.list.gz | egrep \"^[0-9]+\. .*/${1}-[0-9\.]\" | cut -f 2 -d ' ' | tail -n 1`"
+	if [ -z "$res" ]; then
+		echo "Package not found $1" 1>&2
+		exit 1
+	fi
+	basename "$res" | sed -e "s/${1}-/${1} /g" -e "s/\.tar\.bz2//g" -e "s/\.tar\.gz//g" -e "s/\.zip//g" -e "s/\.tgz//g" -e "s/\.tar.xz//g"
+}
+
+######################### SECTION ############################
+#{name} {version}
+function yumFindFileName()
+{
+	res="`zcat ${SV_HOME_LINUX_SHARED}/distfile.list.gz | egrep \"^[0-9]+\. .*/${1}-${2}\" | cut -f 2 -d ' ' | tail -n 1`"
+	basename "$res"
+}
+
---
+++ lib/svhomelinux/svpack-basic.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+##############################################################
+#                                                            #
+#         PROJECT : svHomeLinux                              #
+#         VERSION : 1.1                                      #
+#         DATE    : jeu. févr.  3 18:53:24 CET 2011          #
+#         AUTHORS : Sébastien Valat                          #
+#         LICENCE : GPLv2                                    #
+#                                                            #
+##############################################################
+
+######################### SECTION ############################
+PACKAGE_NAME=''
+PACKAGE_VERSION=''
+PACKAGE_ARCHIVE=''
+PACKAGE_SUBDIR=''
+PACKAGE_VARIANT=''
+PACKAGE_CONFIGURE_OPTIONS=''
+PACKAGE_TEST_TARGET=''
+
+######################### SECTION ############################
+function do_check_vars()
+{
+	if [ -z "$PACKAGE_NAME" ]; then pack_var_required PACKAGE_NAME; fi
+	if [ -z "$PACKAGE_VERSION" ]; then pack_var_required PACKAGE_VERSION; fi
+	if [ -z "$PACKAGE_ARCHIVE" ]; then pack_var_required PACKAGE_ARCHIVE; fi
+}
+
+######################### SECTION ############################
+function do_download()
+{
+	for serv in ${GENTOO_MIRROR}
+	do
+		if [ ! -f "${SV_HOME_LINUX_SHARED}/distfiles/${PACKAGE_ARCHIVE}" ]; then
+			echo ">>> Downloading ${PACKAGE_ARCHIVE} <<<"
+			wget "$serv/distfiles/${PACKAGE_ARCHIVE}" -O ${SV_HOME_LINUX_SHARED}/distfiles/${PACKAGE_ARCHIVE}
+		fi
+	done
+
+	if [ ! -f "${SV_HOME_LINUX_SHARED}/distfiles/${PACKAGE_ARCHIVE}" ]; then
+		echo "Error: can't download archive file"
+		exit 1;
+	fi
+}
+
+######################### SECTION ############################
+function do_extract()
+{
+	case "$PACKAGE_ARCHIVE" in
+		*.tar.xz)
+			echo "xz -d -c \"${SV_HOME_LINUX_SHARED}/distfiles/${PACKAGE_ARCHIVE}\" | tar -x"
+			xz -d -c "${SV_HOME_LINUX_SHARED}/distfiles/${PACKAGE_ARCHIVE}" | tar -x || exit 1
+			;;
+		*.tar.gz)
+			safe_exec tar -xzf "${SV_HOME_LINUX_SHARED}/distfiles/${PACKAGE_ARCHIVE}"
+			;;
+		*.tgz)
+			safe_exec tar -xzf "${SV_HOME_LINUX_SHARED}/distfiles/${PACKAGE_ARCHIVE}"
+			;;
+		*.tar.bz2)
+			safe_exec tar -xjf "${SV_HOME_LINUX_SHARED}/distfiles/${PACKAGE_ARCHIVE}"
+			;;
+		*)
+			echo "Error: unsupported format $PACKAGE_ARCHIVE"
+			exit 1
+			;;
+	esac
+}
+
+######################### SECTION ############################
+function  do_move_to_build_dir()
+{
+	echo ">>> Moving to subdir ${PACKAGE_SUBDIR} <<<"
+	safe_exec cd "${PACKAGE_SUBDIR}"
+}
+
+######################### SECTION ############################
+function do_configure()
+{
+	if [ -z "$PACKAGE_VARIANT" ]; then
+		safe_exec svconfigure ${PACKAGE_CONFIGURE_OPTIONS}
+	else
+		safe_exec svconfigure --variant "$PACKAGE_VARIANT" ${PACKAGE_CONFIGURE_OPTIONS}
+	fi
+}
+
+######################### SECTION ############################
+function do_test()
+{
+	if [ ! -z "${PACKAGE_TEST_TARGET}" ]; then
+		echo ">>> Execute tests <<<"
+		current="$PWD"
+		if [ -d svbuild ]; then safe_exec cd svbuild ; fi
+		safe_exec make
+		safe_exec cd "$current"
+	fi
+}
+
+######################### SECTION ############################
+function do_install()
+{
+	safe_exec svmakeinstall "$PACKAGE_NAME" "$PACKAGE_VERSION"
+}
+
+######################### SECTION ############################
+function do_clean()
+{
+	echo "Nothing to do"
+}
