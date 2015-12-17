72c6c3a35c426315d5be5dac997f15efd76921c0
Sebastien Valat <sebastien.valat.dev@orange.fr>
2015-12-17 22:32:41 +0100

    * Add --help to hl command

--- bin/homelinux.sh
+++ bin/homelinux.sh
@@ -4,8 +4,30 @@ set -e
 HLPATH=$(dirname $0)
 cd $HLPATH/../lib/homelinux/
 
-if [ "$1" = "build-cache" ]; then
-	hl-build-cache
-else
-	node index.js "$@"
-fi
+HELP="hl {COMMAND} [OPTIONS]
+
+Commands:
+	build-cache      : Rebuild the cache file to convert short 
+	                   package name to long names.
+	help             : Show this help message.
+	install          : Install the requested packages. Require package 
+	                   name as argument.
+	gen-package      : Pint generated package.
+	gen-full-package : Print fully generated package.
+	gen-install      : Generate install script to forward to bash.
+	update-db        : Sync the gentoo DB for unknown packages.
+	fetch-versions   : Fetch versions of given package (work only
+	                   for db packages.
+"
+
+case "$1" in
+	"build-cache")
+		hl-build-cache
+		;;
+	"help"|"--help"|"-h")
+		echo "$HELP"
+		;;
+	*)
+		node index.js "$@"
+		;;
+esac
