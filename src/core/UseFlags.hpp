cfdcd0d8a1108e21def82013ebc93059b68ba49d
Sebastien Valat <sebastien.valat-dev@orange.fr>
2017-06-16 09:06:28 +0000

    [core] Start to add UseFlags (to be implemented)

---
+++ src/core/UseFlags.hpp
@@ -0,0 +1,28 @@
+/*****************************************************
+             PROJECT  : homelinux
+             VERSION  : 2.0.0-dev
+             DATE     : 06/2017
+             AUTHOR   : Valat Sébastien
+             LICENSE  : CeCILL-C
+*****************************************************/
+
+#ifndef HL_USE_FLAGS_HPP
+#define HL_USE_FLAGS_HPP
+
+/********************  HEADERS  *********************/
+
+/*******************  NAMESPACE  ********************/
+namespace hl
+{
+
+/********************  STRUCT  **********************/
+/**
+ * Structure to store the configuration of HLPIPE.
+**/
+struct UseFlags
+{
+};
+
+}
+
+#endif //HL_USE_FLAGS_HPP
